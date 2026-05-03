72b8b550ba09af9abff2253517bb3cd78146dd71
Nikolay <mutovin36@gmail.com>
2026-05-03 22:25:04 +0300

    app-deadline.jar3

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,9 +1,4 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users
-(
-    id       CHAR(36) PRIMARY KEY,
-    login    VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)        NOT NULL,
+password VARCHAR(255)        NOT NULL,
     status   VARCHAR(255)        NOT NULL DEFAULT 'active'
 );
 
